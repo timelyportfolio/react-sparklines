dcf7188f2cb262a659fb9a42904e88b885416a05
timelyportfolio <kent.russell@timelyportfolio.com>
2016-08-07 20:58:54 -0500

    add example with data from R

--- R/example.R
+++ R/example.R
@@ -3,20 +3,28 @@ library(htmltools)
 
 browsable(attachDependencies(
   tagList(
-    tags$div(id="sparkline", style="height:60px;width:150px"),
-    tags$script(babel_transform(
-  "
-  var Sparklines = ReactSparklines.Sparklines;
-  var SparklinesLine = ReactSparklines.SparklinesLine;
-  
-  ReactDOM.render(
-    <Sparklines data={[1,2,3,4,5]}>
-      <SparklinesLine />
-    </Sparklines>,
-    document.getElementById('sparkline')
-  );
-  "
-    ))
+    tags$div(id="sparkline1", style="height:60px;width:150px"),
+    tags$div(id="sparkline2", style="height:60px;width:150px"),
+    tags$script(babel_transform(sprintf(
+"
+var Sparklines = ReactSparklines.Sparklines;
+var SparklinesLine = ReactSparklines.SparklinesLine;
+
+ReactDOM.render(
+  <Sparklines data={[1,2,3,4,5]}>
+    <SparklinesLine />
+  </Sparklines>,
+  document.getElementById('sparkline1')
+);
+
+ReactDOM.render(
+  <Sparklines data={%s}>
+    <SparklinesLine />
+  </Sparklines>,
+  document.getElementById('sparkline2')
+);
+"
+    ,jsonlite::toJSON(runif(100)))))
   ),
   list(
     html_dependency_react(),
